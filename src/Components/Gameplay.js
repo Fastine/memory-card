530c3e08d3a7ab225d68605ec0aab22bf774a03c
JBe <jmbeaderstadt@gmail.com>
2022-06-27 18:16:11 -0500

    Refactor image array - board now maps images

--- src/Components/Gameplay.js
+++ src/Components/Gameplay.js
@@ -1,31 +1,84 @@
 import React from 'react';
-import Images from "../Images";
+import '../index.css';
 
 function Gameplay() {
 
-    // 
-
+  var gameboard = [
+    {
+      name: "Astrologian",
+      id: 1
+    },
+    {
+      name: "Bard",
+      id: 2
+    },
+    {
+      name: "BlackMage",
+      id: 3
+    },
+    {
+      name: "BlueMage",
+      id: 4
+    },
+    {
+      name: "Dancer",
+      id: 5
+    },
+    {
+      name: "DarkKnight",
+      id: 6
+    },
+    {
+      name: "Dragoon",
+      id: 7
+    },
+    {
+      name: "Gunbreaker",
+      id: 8
+    },
+    {
+      name: "Machinist",
+      id: 9
+    },
+    {
+      name: "Monk",
+      id: 10
+    },
+    {
+      name: "Ninja",
+      id: 11
+    },
+    {
+      name: "Paladin",
+      id: 12
+    },
+    {
+      name: "RedMage",
+      id: 13
+    },
+    {
+      name: "Samurai",
+      id: 14
+    },
+    {
+      name: "Scholar",
+      id: 15
+    },
+    {
+      name: "Summoner",
+      id: 16
+    },
+    {
+      name: "Warrior",
+      id: 17
+    }
+  ]
 
   return (
-    <div>
-      <input type="image" src={Images.Astrologian} alt="Astrologian" />
-      <input type="image" src={Images.Bard} alt="Bard" />
-      <input type="image" src={Images.BlackMage} alt="BlackMage" />
-      <input type="image" src={Images.BlueMage} alt="BlueMage" />
-      <input type="image" src={Images.Dancer} alt="Dancer" />
-      <input type="image" src={Images.DarkKnight} alt="DarkKnight" />
-      <input type="image" src={Images.Dragoon} alt="Dragoon" />
-      <input type="image" src={Images.Gunbreaker} alt="Gunbreaker" />
-      <input type="image" src={Images.Machinist} alt="Machinist" />
-      <input type="image" src={Images.Monk} alt="Monk" />
-      <input type="image" src={Images.Ninja} alt="Ninja" />
-      <input type="image" src={Images.Paladin} alt="Paladin" />
-      <input type="image" src={Images.RedMage} alt="RedMage" />
-      <input type="image" src={Images.Samurai} alt="Samurai" />
-      <input type="image" src={Images.Scholar} alt="Scholar" />
-      <input type="image" src={Images.Summoner} alt="Summoner" />
-      <input type="image" src={Images.Warrior} alt="Warrior" />
-      <input type="image" src={Images.WhiteMage} alt="WhiteMage" />
+    <div className="gameboard">
+      {gameboard.map((currentValue) => {
+        return <input type="image" src={require("../Img/" + currentValue.name + ".png")} alt={currentValue} key={currentValue.id} />
+      })}
     </div>
   );
 };
--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../index.css"
 
 const Scoreboard = (props) => {
     
     return(
-        <div>
+        <div className="scoreboard">
             <span>Current Score: <span>{props.currentScore}</span></span>
             <br />
             <span>High Score: <span>{props.hiScore}</span></span>
